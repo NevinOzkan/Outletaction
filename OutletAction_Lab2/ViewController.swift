b7ad0d5675c1defa050e6c3e81765cedf1cec0b0
NevinOzkan <ozkannevin73@gmail.com>
2023-02-27 21:21:56 +0300

    Initial Commit

---
+++ OutletAction_Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutletAction_Lab2
+//
+//  Created by Nevin Özkan on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
